7d44adc071be81b5174ecc7a562da10d69b8bd43
Luis González <lgonzalez@teravisiontech.com>
2017-06-30 17:50:55 -0400

    Functional search engine

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import debounce from 'lodash/debounce'
 
 class SearchBar extends Component {
     constructor() {
-        super();
+        super()
         this.state = { term: '' }
     }
 
-    onInputChange(term) {
-        this.setState({term});
-        this.props.onTermChange(term);
-    }
+    onInputChange = debounce((term) => {
+        this.setState({term})
+        this.props.onTermChange(term)
+    }, 200)
 
     render() {
         return (
             <div className="search">
-                <input onChange={event => this.onInputChange(event.target.value)} />
+                <input placeholder="Enter text to search for gifs!" onChange={event => this.onInputChange(event.target.value)} />
             </div>
         );
     }
